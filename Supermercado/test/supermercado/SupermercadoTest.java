10bbc786b64b1e5f9583d23a971c8eaed8dfb367
Javier <cabreramoreljavier@gmail.com>
2024-05-22 11:26:47 -0300

    inicio

---
+++ Supermercado/test/supermercado/SupermercadoTest.java
@@ -0,0 +1,81 @@
+package supermercado;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SupermercadoTest {
+
+
+	
+	@Test
+	public void queSePuedaCrearCrearCliente() {
+		
+	}
+
+	@Test
+	public void queSePuedaAgregarProductoAlInventario() {
+	    // Implementación del test
+	}
+
+	@Test
+	public void queSePuedaEliminarProductoDelInventario() {
+	    // Implementación del test
+	}
+	
+	@Test
+	public void queSePuedaActualizarInventario() {
+	    // Implementación del test
+	}
+	
+	@Test
+	public void queSePuedaEliminarCliente() {
+	    // Implementación del test
+	}
+
+	@Test
+	public void queSePuedaAgregarProductoAlCarrito() {
+	    // Implementación del test
+	}
+
+	@Test
+	public void queSePuedaEliminarProductoDelCarrito() {
+	    // Implementación del test
+	}
+	
+	@Test
+	public void queSePuedaCalcularElTotalDelCarrito() {
+	    // Implementación del test
+	}
+
+	@Test
+	public void queSePuedaSeleccionarFormaDePago() {
+	    // Implementación del test
+	}
+
+	@Test
+	public void queSePuedaRealizarVentaYActualizarInventario() {
+	  
+	}
+	
+	
+	@Test
+	public void queSePuedaActualizarSaldoDelClienteDespuesDeLaVenta() {
+	    // Implementación del test
+	}
+	
+	
+	@Test
+	public void queSePuedaActualizarBalanceDeLaTiendaDespuesDeLaVenta() {
+	    // Implementación del test
+	}
+	
+	@Test
+	public void queSePuedaRegistrarVentaEnElSistema() {
+	    // Implementación del test
+	}
+
+
+
+
+}
