a4292692d83050f010898238dbb4e685b416d8b6
francisco-larralde <larraldefrancisco19@gmail.com>
2024-05-24 09:39:07 -0300

    Interfaz

---
+++ ISupermercado.java
@@ -0,0 +1,16 @@
+package supermercado;
+
+import java.util.List;
+
+public interface ISupermercado {
+	
+	Boolean agregarCliente(Cliente cliente);
+	List<Cliente> getClientes();
+	List<ProductoCantidad> getInventario();
+	Boolean eliminarClientePorDni(Integer dni);
+	Boolean agregarProductoAInventario(Producto producto, Integer cantidad);
+	Boolean eliminarProductoPorId(Integer idProducto);
+	boolean agregarProductoAlCarrito(Integer idProducto, Integer dniCliente);
+	boolean eliminarProductoDelCarrito(Integer idProducto, Integer dniCliente);
+	void actualizarPrecio(Double nuevoPrecio, int idProducto);
+}
