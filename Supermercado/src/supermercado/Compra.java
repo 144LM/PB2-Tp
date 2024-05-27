de3173f493a4cf04b66e37beae4e3bd86382f61c
francisco-larralde <larraldefrancisco19@gmail.com>
2024-05-27 10:51:09 -0300

    clase Compra

---
+++ Supermercado/src/supermercado/Compra.java
@@ -0,0 +1,65 @@
+package supermercado;
+
+import java.time.LocalDateTime;
+
+public class Compra {
+
+	private Integer idCompra;
+	private Cliente cliente;
+	private LocalDateTime now;
+	private MetodoPago metodoPago;
+	private EstadoCompra estadoCompra;
+
+	public Compra(Integer idCompra, Cliente cliente, LocalDateTime now, MetodoPago metodoPago, EstadoCompra estadoCompra) {
+		// TODO Auto-generated constructor stub
+		this.idCompra = idCompra;
+		this.cliente = cliente;
+		this.now = now;
+		this.metodoPago = metodoPago;
+		this.estadoCompra = estadoCompra;
+		
+	}
+
+	public Integer getIdCompra() {
+		return idCompra;
+	}
+
+	public void setIdCompra(Integer idCompra) {
+		this.idCompra = idCompra;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public LocalDateTime getNow() {
+		return now;
+	}
+
+	public void setNow(LocalDateTime now) {
+		this.now = now;
+	}
+
+	public MetodoPago getMetodoPago() {
+		return metodoPago;
+	}
+
+	public void setMetodoPago(MetodoPago metodoPago) {
+		this.metodoPago = metodoPago;
+	}
+
+	public EstadoCompra getEstadoCompra() {
+		return estadoCompra;
+	}
+
+	public void setEstadoCompra(EstadoCompra estadoCompra) {
+		this.estadoCompra = estadoCompra;
+	}
+	
+	
+
+}
